2cf902ed8bcf8cc8a5ab1a102580932d650dadc4
Ivailo Vasiliev <ivailo_vasiliev@hotmail.com>
2020-03-18 15:21:35 +0200

    Add project files.

---
+++ BookShop.Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace BookShop.Data
+{
+    internal class Configuration
+    {
+        
+        internal static string ConnectionString => "Server=DESKTOP-4Q4QNM5\\SQLEXPRESS;Database=BookShop;Integrated Security=True;";
+    }
+}
---
+++ BookShop/StartUp.cs
@@ -0,0 +1,37 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using (var db = new BookShopContext())
+            {
+                var result = GetBooksByAgeRestriction(db, "Adult");
+                Console.WriteLine(result);
+            }
+        }
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            var ageRestriction = Enum.Parse<AgeRestriction>(command, true);
+
+            var books = context.Books
+                .Where(a => a.AgeRestriction == ageRestriction)
+                .Select(t => t.Title)
+                .OrderBy(x => x)
+                .ToList();
+
+            var result = string.Join(Environment.NewLine, books);
+
+            return result;        
+        }
+
+       
+    }
+}
